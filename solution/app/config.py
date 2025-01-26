1a81e2427db9b7eef03f0d51149a905755b740e3
IvanShumilov24 <ivansergshum@gmail.com>
2025-01-26 18:05:04 +0300

    Edit config

--- solution/app/config.py
+++ solution/app/config.py
@@ -1,28 +1,30 @@
+import os
+
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
 class Settings(BaseSettings):
-    SERVER_ADDRESS: str
-    SERVER_PORT: int
+    SERVER_ADDRESS: str = os.getenv("SERVER_ADDRESS")
+    SERVER_PORT: int = os.getenv("SERVER_PORT")
 
-    POSTGRES_CONN: str
-    POSTGRES_JDBC_URL: str
-    POSTGRES_USERNAME: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: str
-    POSTGRES_DATABASE: str
+    POSTGRES_CONN: str = os.getenv("POSTGRES_CONN")
+    POSTGRES_JDBC_URL: str = os.getenv("POSTGRES_JDBC_URL")
+    POSTGRES_USERNAME: str = os.getenv("POSTGRES_USERNAME")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    POSTGRES_DATABASE: str = os.getenv("POSTGRES_DATABASE")
 
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}"
 
-    REDIS_HOST: str
-    REDIS_PORT: str
+    REDIS_HOST: str = os.getenv("REDIS_HOST")
+    REDIS_PORT: str = os.getenv("REDIS_PORT")
 
-    ANTIFRAUD_ADDRESS: str
+    ANTIFRAUD_ADDRESS: str = os.getenv("ANTIFRAUD_ADDRESS")
 
-    RANDOM_SECRET: str
+    RANDOM_SECRET: str = os.getenv("RANDOM_SECRET")
 
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
